522bd3ee52b1ae653dce2eba463b0e9ea65c9815
github username <meliajm@gmail.com>
2020-02-27 20:40:47 -0800

    Done.

---
+++ app/validators/title_validator.rb
@@ -0,0 +1,8 @@
+class TitleValidator < ActiveModel::Validator
+    def validate(record)
+    #   unless record.title.include?("Won't Believe") || record.title.include?("Secret") || record.title.include?("Guess") || record.title.include?("Top ")
+      if ["Won't Believe", "Secrect", "Guess", "Top"].any? { |s| !record.title.include? s }
+        record.errors[:name] << "Not click bait enough"
+      end
+    end
+end
