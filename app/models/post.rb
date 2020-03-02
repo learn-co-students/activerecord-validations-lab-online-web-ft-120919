c0566d1804f73eba73c72d069580fcbdc8b18cea
Adrian <mohabiradrian@gmail.com>
2020-03-02 16:12:09 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction || Non-Fiction)}
+    validate :clickbait
+    # include ActiveModel::Validations
+    # validates_with MyValidator
+
+    def clickbait
+        unless !title.nil? && title.match?(/(Won't Believe|Secret|Top D+|Guess)/)
+            errors.add :title, "must be clickbait!"
+        end 
+    end 
 end
