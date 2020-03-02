1980bbf97a0377a87b3936d8f08332eb333e0e8a
nwscala <nwscala@gmail.com>
2020-03-02 11:20:29 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :clickbait_title
+
+    def clickbait_title
+        if title && (!title.include?("You Won't" || "Secret" || "Top" || "Guess"))
+            errors.add(:clickbait_title, "That title isn't clickbaity enough.")
+        end
+    end
 end
