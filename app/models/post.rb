ac2d567d377cb712e1e5092c8bacdb66380f8882
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-03-02 14:21:57 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+require "pry"
+
 class Post < ActiveRecord::Base
+
+validates :title, presence: true 
+validates :content, length: { minimum: 250 }
+validates :summary, length: { maximum: 250 }
+validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+validate :set_clickbait
+def set_clickbait
+    # binding.pry
+# if !title.match(/Won't Believe|Secret|Top [0-9]*|Guess/)
+#     errors.add(:title, "clickbait")
+# end
+
+if !(/Won't Believe|Secret|Top [0-9]*|Guess/).match(title)
+    errors.add(:title, "clickbait")
+   end
+end 
+
 end
