dce8ac7cab21d5ea98ce1879aaaff0a33c052608
Uchoosecode <awittyl44@gmail.com>
2020-03-16 08:42:35 -0400

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,7 +1,7 @@
 class TitleValidator < ActiveModel::Validator
     def validate(record)
         unless record.title.include?("Won't Believe", "Secret", "Top [number]", "Guess")
-            record.add[:title] << "You Won't Believe These True Facts"
+            record.errors.add[:title] << "You Won't Believe These True Facts"
         end
     end
 end
