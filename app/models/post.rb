e2f20c7148599be69e84a7fa350b642a405f81eb
Uchoosecode <awittyl44@gmail.com>
2020-03-16 14:42:56 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,15 @@
 class Post < ActiveRecord::Base
-  
-    include ActiveModel::Validations
-    validates_with TitleValidator
 
     validates :title, presence: true
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
-   
+    validate :worthy
+
+        def worthy
+          if !title.nil? && (!title.include?("You Won't" || "Secret" || "Top" || "Guess")) 
+            errors.add(:worthy, "That is not a clickbait worthy title. Try again.")
+          end
+        end
+
 end
--- app/validators/title_validator.rb
+++
@@ -1,8 +0,0 @@
-class TitleValidator < ActiveModel::Validator
-    def validate(record)
-        unless record.title.match?("Won't Believe", "Secret", "Top [number]", "Guess")
-            binding.pry
-            record.errors.add :title, "Invalid not clickbait"
-        end
-    end
-end
